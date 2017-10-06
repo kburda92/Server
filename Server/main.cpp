cd05923a9d02ed50919f66a29845af55f6e4e4bd
krzysiak <krzysiak1992@gmail.com>
2017-10-06 21:04:37 +0200

    c++ rest sdk

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,33 @@
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
 
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
 
 
 int main()
 {
-	return 0;
+	http_listener listener(L"http://localhost/restdemo");
+
+	//listener.support(methods::GET, handle_get);
+	//listener.support(methods::POST, handle_post);
+	//listener.support(methods::PUT, handle_put);
+	//listener.support(methods::DEL, handle_del);
+
+	//try
+	//{
+	//	listener
+	//		.open()
+	//		.then([&listener]() {TRACE(L"\nstarting to listen\n"); })
+	//		.wait();
+
+	//	while (true);
+	//}
+	//catch (exception const & e)
+	//{
+	//	wcout << e.what() << endl;
+	//}
+
+	//return 0;
 }
